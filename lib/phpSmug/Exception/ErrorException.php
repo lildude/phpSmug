8531254c485477af6f04d431945178d44d5a9fe0
Colin Seymour <lildood@gmail.com>
2014-12-03 15:58:49 +0000

    Add exception

---
+++ lib/phpSmug/Exception/ErrorException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace phpSmug\Exception;
+
+/**
+ * ErrorException
+ *
+ * @author Joseph Bielawski <stloyd@gmail.com>
+ */
+class ErrorException extends \ErrorException implements ExceptionInterface
+{
+
+}
---
+++ lib/phpSmug/Exception/RuntimeException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace phpSmug\Exception;
+
+/**
+ * RuntimeException
+ *
+ * @author Joseph Bielawski <stloyd@gmail.com>
+ */
+class RuntimeException extends \RuntimeException implements ExceptionInterface
+{
+
+}
