af70951281ead50fb6444bcc7e4d827c7def7b37
Colin Seymour <lildood@gmail.com>
2014-12-03 18:50:20 +0000

    Add rest of the plumbing

--- lib/phpSmug/Client.php
+++ lib/phpSmug/Client.php
@@ -64,6 +64,93 @@ class Client
 
         return $api;
     }
+
+        /**
+     * @return HttpClient
+     */
+    public function getHttpClient()
+    {
+        if (null === $this->httpClient) {
+            $this->httpClient = new HttpClient($this->options);
+        }
+
+        return $this->httpClient;
+    }
+
+    /**
+     * @param HttpClientInterface $httpClient
+     */
+    public function setHttpClient(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * Clears used headers
+     */
+    public function clearHeaders()
+    {
+        $this->getHttpClient()->clearHeaders();
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->getHttpClient()->setHeaders($headers);
+    }
+
+        /**
+     * @param string $name
+     *
+     * @return mixed
+     *
+     * @throws InvalidArgumentException
+     */
+    public function getOption($name)
+    {
+        if (!array_key_exists($name, $this->options)) {
+            throw new InvalidArgumentException(sprintf('Undefined option called: "%s"', $name));
+        }
+
+        return $this->options[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     *
+     * @throws InvalidArgumentException
+     * @throws InvalidArgumentException
+     */
+    public function setOption($name, $value)
+    {
+        if (!array_key_exists($name, $this->options)) {
+            throw new InvalidArgumentException(sprintf('Undefined option called: "%s"', $name));
+        }
+        $supportedApiVersions = $this->getSupportedApiVersions();
+        if ('api_version' == $name && !in_array($value, $supportedApiVersions)) {
+            throw new InvalidArgumentException(sprintf('Invalid API version ("%s"), valid are: %s', $name, implode(', ', $supportedApiVersions)));
+        }
+
+        $this->options[$name] = $value;
+    }
+
+        /**
+     * @param string $name
+     *
+     * @return ApiInterface
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __call($name, $args) {
+        try {
+            return $this->api($name);
+        } catch (InvalidArgumentException $e) {
+            throw new BadMethodCallException(sprintf('Undefined method called: "%s"', $name));
+        }
+    }
 }
 
 ?>
