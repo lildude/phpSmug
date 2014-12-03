e615f2d5c60763685ead36b1433f26aa1b12550e
Colin Seymour <colin@github.com>
2014-12-03 15:58:49 +0000

    Add exception

---
+++ lib/phpSmug/Exception/BadMethodCallException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace phpSmug\Exception;
+
+/**
+ * BadMethodCallException
+ *
+ * @author James Brooks <jbrooksuk@me.com>
+ */
+class BadMethodCallException extends \BadMethodCallException implements ExceptionInterface
+{
+
+}
---
+++ lib/phpSmug/Exception/ValidationFailedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace phpSmug\Exception;
+
+/**
+ * ValidationFailedException
+ *
+ * @author Joseph Bielawski <stloyd@gmail.com>
+ */
+class ValidationFailedException extends ErrorException
+{
+
+}
