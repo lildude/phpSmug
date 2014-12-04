da054517f12f1644523f34eb1c6656fa18f9f8c1
Colin Seymour <colin@github.com>
2014-12-04 17:34:52 +0000

    Added ClientTest file

---
+++ test/phpSmug/Tests/ClientTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace phpSmug\Tests;
+
+use phpSmug\Client;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldNotHaveToPassHttpClientToConstructor()
+    {
+        $client = new Client();
+
+        $this->assertInstanceOf('phpSmug\HttpClient\HttpClient', $client->getHttpClient());
+    }
+}
+
+?>
