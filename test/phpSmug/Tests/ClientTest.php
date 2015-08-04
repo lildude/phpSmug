eacf6986dc27e9b97246fa64ecc72fd9e3c86d66
Colin Seymour <lildood@gmail.com>
2015-08-04 14:02:43 +0100

    Add shouldPassHttpClientInterfaceToConstructor test and mock function

--- test/phpSmug/Tests/ClientTest.php
+++ test/phpSmug/Tests/ClientTest.php
@@ -15,7 +15,30 @@ class ClientTest extends \PHPUnit_Framework_TestCase
       $this->assertInstanceOf('phpSmug\HttpClient\HttpClient', $client->getHttpClient());
     }
 
-    
+    /**
+     * @test
+     */
+    public function shouldPassHttpClientInterfaceToConstructor()
+    {
+        $client = new Client($this->getHttpClientMock());
+
+        $this->assertInstanceOf('phpSmug\HttpClient\HttpClientInterface', $client->getHttpClient());
+    }
+
+
+
+
+
+
+    public function getHttpClientMock(array $methods = array())
+    {
+        $methods = array_merge(
+            array('get', 'post', 'patch', 'put', 'delete', 'request', 'setOption', 'setHeaders', 'authenticate'),
+            $methods
+        );
+
+        return $this->getMock('phpSmug\HttpClient\HttpClientInterface', $methods);
+    }
 }
 
 ?>
