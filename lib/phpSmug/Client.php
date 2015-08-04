832c043fb269af25aa016840cb95e74605ef1427
Colin Seymour <lildood@gmail.com>
2015-08-04 14:12:57 +0100

    Add remaining client funcs and start on api()

--- lib/phpSmug/Client.php
+++ lib/phpSmug/Client.php
@@ -8,6 +8,11 @@ use phpSmug\HttpClient\HttpClientInterface;
 class Client
 {
     /**
+     * Constant for the API Key. Get your API Key from https://api.smugmug.com/api/developer/apply
+     */
+    const API_KEY = 'api_key';
+
+    /**
      * @var array
      */
     private $options = array(
@@ -39,6 +44,27 @@ class Client
         $this->httpClient = $httpClient;
     }
 
+    /**
+     * @param string $name
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return ApiInterface
+     */
+    public function api($name)
+    {
+        switch ($name) {
+            case 'me':
+                $api = new Api\CurrentUser($this);
+                break;
+
+        default:
+            throw new InvalidArgumentException(sprintf('Undefined api instance called: "%s"', $name));
+
+        }
+
+        return $api;
+    }
 
     /**
      * @return HttpClient
@@ -50,6 +76,78 @@ class Client
         }
 
         return $this->httpClient;
+    }
+
+    /**
+     * @param HttpClientInterface $httpClient
+     */
+    public function setHttpClient(HttpClientInterface $httpClient)
+    {
+        $this->httpClient = $httpClient;
+    }
+
+    /**
+     * Clears used headers.
+     */
+    public function clearHeaders()
+    {
+        $this->getHttpClient()->clearHeaders();
+    }
+
+    /**
+     * @param array $headers
+     */
+    public function setHeaders(array $headers)
+    {
+        $this->getHttpClient()->setHeaders($headers);
+    }
+
+    /**
+     * @param string $name
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return mixed
+     */
+    public function getOption($name)
+    {
+        if (!array_key_exists($name, $this->options)) {
+            throw new InvalidArgumentException(sprintf('Undefined option called: "%s"', $name));
+        }
+
+        return $this->options[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     *
+     * @throws InvalidArgumentException
+     * @throws InvalidArgumentException
+     */
+    public function setOption($name, $value)
+    {
+        if (!array_key_exists($name, $this->options)) {
+            throw new InvalidArgumentException(sprintf('Undefined option called: "%s"', $name));
+        }
+
+        $this->options[$name] = $value;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @throws InvalidArgumentException
+     *
+     * @return ApiInterface
+     */
+    public function __call($name, $args)
+    {
+        try {
+            return $this->api($name);
+        } catch (InvalidArgumentException $e) {
+            throw new BadMethodCallException(sprintf('Undefined method called: "%s"', $name));
+        }
     }
 }
 
