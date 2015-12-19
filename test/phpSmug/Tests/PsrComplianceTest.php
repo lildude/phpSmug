7a1f3f8121dd6cfe94683d0e8bbdadaa607f961f
Colin Seymour <lildood@gmail.com>
2015-12-19 11:27:38 +0000

    Rename PSR compliance test and remove unused code

--- test/phpSmug/Tests/PsrComplianceTest.php
+++ test/phpSmug/Tests/PsrComplianceTest.php
@@ -11,7 +11,7 @@ class PsrComplianceTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function testPSR()
+    public function testPsrCompliance()
     {
         // If we can't find the command-line tool, we mark the test as skipped
         // so it shows as a warning to the developer rather than passing silently.
@@ -20,33 +20,7 @@ class PsrComplianceTest extends \PHPUnit_Framework_TestCase
                 'Needs linter to check PSR compliance'
             );
         }
-
-        // Let's check all PSR compliance for our code and tests.
-        // Add any other pass you want to test to this array.
-        /*foreach (array('lib/', 'examples/', 'test/') as $path) {
-            // Run linter in dry-run mode so it changes nothing.
-            exec(
-                escapeshellcmd('vendor/bin/php-cs-fixer fix --diff -v --dry-run '.$_SERVER['PWD']."/$path"),
-                $output,
-                $return_var
-            );
-
-            // If we've got output, pop its first item ("Fixed all files...")
-            // shift off the last two lines, and trim whitespace from the rest.
-            if ($output) {
-                array_pop($output);
-                array_shift($output);
-                array_shift($output);
-                $output = array_map('trim', $output);
-            }
-
-            // Check shell return code: if nonzero, report the output as a failure.
-            $this->assertEquals(
-                0,
-                $return_var,
-                "PSR linter reported errors in $path: \n\t".implode("\n\t", $output)
-            );
-        }*/
+        
         // Run linter in dry-run mode so it changes nothing.
         exec(
             escapeshellcmd('vendor/bin/php-cs-fixer fix --diff -v --dry-run .'),
